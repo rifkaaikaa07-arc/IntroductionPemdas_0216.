4612e0c1ebbcaed3f55102c3b0c71bfb01b30425
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-02 20:48:40 +0700

    Menambahkan input

--- parameter.cpp
+++ parameter.cpp
@@ -1,3 +1,13 @@
 //library
 #include <iostream>
 using namespace std;
+
+//deklarasi variabel global
+int panjang, lebar;
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "masukkan panjang : ";
+    cin >> panjang;
+    cout << "masukkan lebar : ";
+    cin >> lebar;
+}
