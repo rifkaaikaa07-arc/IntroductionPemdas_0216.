bb90ac6ed0a896954593e7a6bcf96c4f99105217
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-02 21:07:37 +0700

    Menambahkan input data diagonal

--- prosedur.cpp
+++ prosedur.cpp
@@ -2,3 +2,10 @@
 using namespace std;
 
 float d_1, d_2, Luas;
+
+void prosedurinputdata (){
+    cout << "masukkan diagonal_1 : ";
+    cin >> d_1;
+    cout << "masukkan diagonal_2 : ";
+    cin >> d_2;
+}
